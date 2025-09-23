cb29a3dcde4a0b58c2bf8549193e4c8bb283a79e
julianderson <163586065+JulhoSoares@users.noreply.github.com>
2025-09-23 11:56:35 -0300

    Update admin.py

--- crud/admin.py
+++ crud/admin.py
@@ -1,12 +1,17 @@
+import bcrypt
+
 from db import cursor,conexao
 
 def menu_admin():
      while True:
         opcao = input("1-Inserir, 2-Deletar, 3-Consultar, 4-Voltar Autenticar, 0-Sair, : ")
         if opcao == "1":
-            nome = input("Nome: ")
+            nome = input("Nome :")
+            senha=input("Digite uma senha! :")
             codigo = input("Código: ")
-            cursor.execute("INSERT INTO usuario (nome, codigo) VALUES (%s, %s)", (nome, codigo))
+
+            hash=bcrypt.hashpw(senha.encode("utf-8"),bcrypt.gensalt())
+            cursor.execute("INSERT INTO usuario (nome,hash,codigo) VALUES (%s, %s,%s)", (nome, hash, codigo))
             conexao.commit()
             print("Inserido com sucesso.")
         elif opcao == "2":
@@ -27,8 +32,6 @@ def menu_admin():
             for linha in cursor.fetchall():
                 print(linha)
         elif opcao =="4":
-            #usuario = autenticar()
-            #resul=menu_usuario()
             return "voltar"
         elif opcao == "0":
             print("FIM DO PROGRAMA")
